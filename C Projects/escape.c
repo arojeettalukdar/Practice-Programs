b30ccc92af46510311423e64b4c7f7dce7c9fdae
Arojeet <arojeettaluckdar@gmail.com>
2022-10-26 01:56:08 +0530

    Commiting the Projects of C files for the first time

---
+++ C Projects/escape.c
@@ -0,0 +1,15 @@
+//escape operator
+#include <stdio.h>
+#include<conio.h>
+ int main()
+{
+    printf("Kiit School of Computer Aplication");
+    /* \n To move the cursor to the next line*/
+    /* \t To move the cursor to the next Tab*/
+    printf("\n8RWF+FCF, \tKOEL Campus");
+    printf("\nNandankanan Rd");
+    printf("\nBhubaneswar");
+    printf("\nOdisha 751024");
+    /* to view the result without using alt+F5 */
+    getch();
+}
